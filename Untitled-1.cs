b572bd76905c231f6fd63688ac64bdcba5077a65
markjhonleano <135992319+markjhonleano@users.noreply.github.com>
2023-06-08 23:33:42 +0800

    Add files via upload

---
+++ Untitled-1.cs
@@ -0,0 +1,19 @@
+<! DOCTYPE html >
+<html>
+<head>
+<link rel ="stylesheet" href="Mj.css">
+</head>
+<body>
+<h1>LOGIN</h1>
+<text-align:center;>
+<form action ="home.html">
+<div class ="container">
+    <label>Username</label>
+    <input type="text" placeholder="Enter Username">
+    <label>Password</label>
+    <input type="Password" placeholder="Enter Password">
+	<button>Login</button>
+</div>
+</form>
+
+</body>
